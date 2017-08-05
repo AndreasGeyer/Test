0e67dccafe513ef9a97223bbd0e76d47b260465c
Andi <ang676@thi.de>
2017-08-05 15:56:52 +0200

    Code Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { FusekiService } from './services/fuseki.service';
 import { VerbNormalisationService } from './services/verbnormalisation.service';
-import { ChangeDetectorRef } from '@angular/core';
-import { NgZone } from '@angular/core';
-import { ChangeDetectionStrategy } from '@angular/core';
 import { Word } from './word';
 import { OnInit } from '@angular/core';
 import { FusekiResult } from './entity';
@@ -14,51 +11,19 @@ import { Complete } from './fuseki';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  words: Word[];
-  public result: any = {
-    header: [{ title: 'Test', name: 'test' }],
-    data: []
-  };
-  public rows: Array<any> = [];
-  public columns: Array<any> = [
-    { title: 'Name', name: 'name' },
-    { title: 'Office', name: 'office' }
-  ];
-  public page: number = 1;
-  public itemsPerPage: number = 10;
-  public maxSize: number = 5;
-  public numPages: number = 1;
-  public length: number = 0;
-
-  public config: any = {
-    paging: true,
-    sorting: { columns: this.columns },
-    filtering: { filterString: '' },
-    className: ['table-striped', 'table-bordered']
-  };
-
-  tableData: Complete[];
-  //let filteredData = this.changeFilter(this.data, this.conf);
-  //let sortedData = this.changeSort(filteredData, this.config);
-  // this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-  //this.length = sortedData.length;
-
-  /*  public itemStringsLeft: any[] = [
-      'Windstorm',
-      'Bombasto',
-      'Magneta',
-      'Tornado'
-    ];
-  
-    public itemStringsRight: any[] = [
-  
-    ];*/
+export class AppComponent implements OnInit {
+  // Saves all the Data in the "Selection" Area
   selection: Selectors;
+  // Saves all the Data in the "Author" Area
   autor: Autor;
+  // Saves all the Data in the "Tweet" Area
   tweet: Tweet;
+  // Saves the Data for the Table
+  tableData: Complete[];
+  // Saves the SPARQL Query
   sparqlQuery: string;
-  constructor(private fusekiService: FusekiService, private cd: ChangeDetectorRef, private zone: NgZone, private verbNormalisationService: VerbNormalisationService) {
+
+  constructor(private fusekiService: FusekiService, private verbNormalisationService: VerbNormalisationService) {
     this.selection = {
       limit: '100',
       autorName: true,
@@ -75,15 +40,11 @@ export class AppComponent implements OnInit{
       objectEntity: false,
       keyWord: false
     };
-
-    // Befüllt den Autor
     this.autor = {
       entityNameChosen: ['Unknown'],
       entityNameNotChosen: [],
       name: ''
     };
-    this.loadAutorEntitys();
-
     this.tweet = {
       keyWord: '',
       subject: {
@@ -103,107 +64,96 @@ export class AppComponent implements OnInit{
         word: '',
       }
     };
+    this.sparqlQuery = '';
+  }
+  ngOnInit(): void {
+    this.loadAutorEntitys();
     this.loadSubjectEntities();
     this.loadObjectEntities();
-    this.sparqlQuery = '';
     this.updateSparqlQuery();
   }
 
-  moveValueArray(arrayFrom: string[], arrayTo: string[], value:string){
-    let index: number = arrayFrom.indexOf(value);
-    if (index !== -1) {
-        arrayFrom.splice(index, 1);
-    } 
-    arrayTo.push(value);
-    arrayTo.sort();
-    arrayFrom.sort();
-    this.updateSparqlQuery();
-  }
-  autorChoseNot(event: any){
-    this.moveValueArray(this.autor.entityNameChosen, this.autor.entityNameNotChosen, event.target.value);
-  }
-  autorChose(event: any){
-    this.moveValueArray(this.autor.entityNameNotChosen, this.autor.entityNameChosen, event.target.value);
-  }
-  subjectNounChoseNot(event:any){
-    this.moveValueArray(this.tweet.subject.nounChosen, this.tweet.subject.nounNotChosen, event.target.value);
-  }
-  subjectNounChose(event:any){
-    this.moveValueArray(this.tweet.subject.nounNotChosen, this.tweet.subject.nounChosen, event.target.value);
-  }
-  subjectEntityNameChoseNot(event:any){
-    this.moveValueArray(this.tweet.subject.entityNameChosen, this.tweet.subject.entityNameNotChosen, event.target.value);
-  }
-  subjectEntityNameChose(event:any){
-    this.moveValueArray(this.tweet.subject.entityNameNotChosen, this.tweet.subject.entityNameChosen, event.target.value);
-  }
-  objectNounChoseNot(event:any){
-    this.moveValueArray(this.tweet.object.nounChosen, this.tweet.object.nounNotChosen, event.target.value);
-  }
-  objectNounChose(event:any){
-    this.moveValueArray(this.tweet.object.nounNotChosen, this.tweet.object.nounChosen, event.target.value);
-  }
-  objectEntityNameChoseNot(event:any){
-    this.moveValueArray(this.tweet.object.entityNameChosen, this.tweet.object.entityNameNotChosen, event.target.value);
-  }
-  objectEntityNameChose(event:any){
-    this.moveValueArray(this.tweet.object.entityNameNotChosen, this.tweet.object.entityNameChosen, event.target.value);
-  }
-  ngOnInit(): void{
-    // TODO alle Vorbereitungen aus dem Konsturktor
-  }
-
+  /*
+  * Loads the Entitys from Fuseki Database
+  */
   loadAutorEntitys() {
     let results;
-    this.fusekiService.getEntitys(
-      `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-       PREFIX twtr: <http://example.org/> 
-       SELECT DISTINCT ?entityName
-       WHERE { ?accout a               twtr:TwitterAccount;
-                       twtr:entityName ?entityName.
-       }`
+    this.fusekiService.getEntitys('TwitterAccount'
     ).then(result => {
       for (let binding of result.results.bindings) {
         console.log(binding.entityName.value);
         this.autor.entityNameChosen.push(binding.entityName.value);
       }
-      
     });
   }
   loadSubjectEntities() {
     let results;
-    this.fusekiService.getEntitys(
-      `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-       PREFIX twtr: <http://example.org/> 
-       SELECT DISTINCT ?entityName
-       WHERE { ?accout a               twtr:Subject;
-                       twtr:entityName ?entityName.
-       }`
+    this.fusekiService.getEntitys('Subject'
     ).then(result => {
       for (let binding of result.results.bindings) {
         console.log(binding.entityName.value);
         this.tweet.subject.entityNameChosen.push(binding.entityName.value);
       }
-      
     });
   }
   loadObjectEntities() {
     let results;
-    this.fusekiService.getEntitys(
-      `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-       PREFIX twtr: <http://example.org/> 
-       SELECT DISTINCT ?entityName
-       WHERE { ?accout a               twtr:Object;
-                       twtr:entityName ?entityName.
-       }`
+    this.fusekiService.getEntitys('Object'
     ).then(result => {
       for (let binding of result.results.bindings) {
         console.log(binding.entityName.value);
         this.tweet.object.entityNameChosen.push(binding.entityName.value);
       }
-      
     });
   }
+
+  /*
+  * The Following Functions control the Entity fields. They move and entity from one lsit to the other.
+  */
+  moveValueArray(arrayFrom: string[], arrayTo: string[], value: string) {
+    let index: number = arrayFrom.indexOf(value);
+    if (index !== -1) {
+      arrayFrom.splice(index, 1);
+    }
+    arrayTo.push(value);
+    arrayTo.sort();
+    arrayFrom.sort();
+    this.updateSparqlQuery();
+  }
+  autorChoseNot(event: any) {
+    this.moveValueArray(this.autor.entityNameChosen, this.autor.entityNameNotChosen, event.target.value);
+  }
+  autorChose(event: any) {
+    this.moveValueArray(this.autor.entityNameNotChosen, this.autor.entityNameChosen, event.target.value);
+  }
+  subjectNounChoseNot(event: any) {
+    this.moveValueArray(this.tweet.subject.nounChosen, this.tweet.subject.nounNotChosen, event.target.value);
+  }
+  subjectNounChose(event: any) {
+    this.moveValueArray(this.tweet.subject.nounNotChosen, this.tweet.subject.nounChosen, event.target.value);
+  }
+  subjectEntityNameChoseNot(event: any) {
+    this.moveValueArray(this.tweet.subject.entityNameChosen, this.tweet.subject.entityNameNotChosen, event.target.value);
+  }
+  subjectEntityNameChose(event: any) {
+    this.moveValueArray(this.tweet.subject.entityNameNotChosen, this.tweet.subject.entityNameChosen, event.target.value);
+  }
+  objectNounChoseNot(event: any) {
+    this.moveValueArray(this.tweet.object.nounChosen, this.tweet.object.nounNotChosen, event.target.value);
+  }
+  objectNounChose(event: any) {
+    this.moveValueArray(this.tweet.object.nounNotChosen, this.tweet.object.nounChosen, event.target.value);
+  }
+  objectEntityNameChoseNot(event: any) {
+    this.moveValueArray(this.tweet.object.entityNameChosen, this.tweet.object.entityNameNotChosen, event.target.value);
+  }
+  objectEntityNameChose(event: any) {
+    this.moveValueArray(this.tweet.object.entityNameNotChosen, this.tweet.object.entityNameChosen, event.target.value);
+  }
+
+
+
+
   /*loadObjectEntities() {
     let results;
     this.fusekiService.sendSparqlQuery(
@@ -222,16 +172,16 @@ export class AppComponent implements OnInit{
 
   // QUELLE: https://stackoverflow.com/questions/37435529/angular2-proper-way-to-restrict-text-input-values-e-g-only-numbers
   checkLimit(event: any) {
-    const pattern = /[0-9\+\-\ ]/;
+    const pattern = /[0-9\ ]/;
     let inputChar = String.fromCharCode(event.charCode);
     if (!pattern.test(inputChar)) {
       // invalid character, prevent input
       event.preventDefault();
     }
-    
+
   }
 
-  checkChanges(){
+  checkChanges() {
     this.updateSparqlQuery();
   }
   updateSparqlQuery() {
@@ -439,26 +389,27 @@ LIMIT ` + this.selection.limit;
 
   refreshNormalized(): void {
     this.tweet.verbNormalized = this.tweet.verb;
-    this.verbNormalisationService.getWords().then(word =>{
-            for(let w of word){
-                if(w.conjugation == this.tweet.verb || w.base == this.tweet.verb){
-                    console.log(w.base);
-                    this.tweet.verbNormalized = w.base;
-                    this.updateSparqlQuery();
-                    return;
-                }
-            }
+    this.verbNormalisationService.getWords().then(word => {
+      for (let w of word) {
+        if (w.conjugation == this.tweet.verb || w.base == this.tweet.verb) {
+          console.log(w.base);
+          this.tweet.verbNormalized = w.base;
+          this.updateSparqlQuery();
+          return;
         }
-        );
+      }
+    }
+    );
   }
 
   sendSparql() {
     let results;
     this.fusekiService.sendSparqlQuery(this.sparqlQuery
     ).then(
-      data => {this.tableData = data;
+      data => {
+      this.tableData = data;
         console.log(this.tableData);
-    });
+      });
   }
 }
 interface Autor {
--- src/app/services/fuseki.service.ts
+++ src/app/services/fuseki.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import {Headers, RequestOptions} from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
 import { Complete } from '../fuseki';
 import { FusekiResult } from '../entity';
 import 'rxjs/add/operator/toPromise';
@@ -9,40 +9,40 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class FusekiService {
 
+    serverUrl: string;
     constructor(private http: Http) {
         console.log('FusekiService Initialized');
+        this.serverUrl = 'http://192.168.99.100:3030/tdb/query';
     }
 
     /*
     Sends a SPQARL Query to Fuseki Server
     */
-    sendSparqlQuery(query:string): Promise<Complete[]>{
-        let headers = new Headers({'Accept': 'application/sparql-results+json,*/*;q=0.9'});
+    sendSparqlQuery(query: string): Promise<Complete[]> {
+        let headers = new Headers({ 'Accept': 'application/sparql-results+json,*/*;q=0.9' });
         headers.append('X-Requested-With', 'XMLHttpRequest');
-        
         headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        let options = new RequestOptions({headers:headers});
-    
-        //return this.http.post('http://192.168.99.100:3030/tdb/query', 'query=' + query, 
-       // options).toPromise().then(this.extractData);
-       let results:Array<Object>;
-       return this.http.post('http://192.168.99.100:3030/tdb/query', 'query=' + query, 
-       options).toPromise().then(this.extractData).catch(this.handleError);
-   
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post(this.serverUrl, 'query=' + query,
+            options).toPromise().then(this.extractData).catch(this.handleError);
+
     }
 
-    getEntitys(query:string):Promise<FusekiResult>{
-        let headers = new Headers({'Accept': 'application/sparql-results+json,*/*;q=0.9'});
+    getEntitys(type: string): Promise<FusekiResult> {
+        let headers = new Headers({ 'Accept': 'application/sparql-results+json,*/*;q=0.9' });
         headers.append('X-Requested-With', 'XMLHttpRequest');
-        
         headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        let options = new RequestOptions({headers:headers});
-    
-        //return this.http.post('http://192.168.99.100:3030/tdb/query', 'query=' + query, 
-       // options).toPromise().then(this.extractData);
-       let results:Array<Object>;
-       return this.http.post('http://192.168.99.100:3030/tdb/query', 'query=' + query, 
-       options).toPromise().then(this.extractData).catch(this.handleError);
+        let options = new RequestOptions({ headers: headers });
+        let query: string;
+        query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+                 PREFIX twtr: <http://example.org/> 
+                 SELECT DISTINCT ?entityName
+                 WHERE { ?accout a               twtr:` + type + `;
+                                 twtr:entityName ?entityName.
+                 }`;
+        return this.http.post(http://192.168.99.100:3030/tdb/query, 'query=' + query,
+            options).toPromise().then(this.extractData).catch(this.handleError);
 
     }
 
@@ -50,9 +50,10 @@ export class FusekiService {
         let body = res.json();
         return body;
     }
+    
     private handleError(error: any): Promise<any> {
         console.log("LOL");
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 }
